191be6f71a288ddb438fa42a04a101fee6772038
nonespl0it <56004379+nonespl0it@users.noreply.github.com>
2019-09-30 19:01:58 -0400

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -1303,14 +1303,13 @@ vg_prefix_context_add_patterns(vg_context_t *vcp,
 							  ranges, NULL);
 			}
 
-		} else {
-			/* Case-enumerate the prefix */
+		} /*else {
 			if (!prefix_case_iter_init(&caseiter, patterns[i])) {
-				fprintf(stderr,
-					"Prefix '%s' is too long\n",
-					patterns[i]);
-				continue;
-			}
+			*	fprintf(stderr,
+			*		"Prefix '%s' is too long\n",
+			*		patterns[i]);
+			*	continue;
+			*/}
 
 			if (caseiter.ci_nbits > 16) {
 				fprintf(stderr,
